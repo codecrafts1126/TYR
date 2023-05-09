6ef85a8f9eb22cd22b0f0fea3442055bb49d69c8
codecrafts1126 <codecrafts1126@gmail.com>
2023-05-09 18:08:31 +0500

    Login Screen

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:tyr/components/textfield.dart';
 
 class Login extends StatefulWidget {
   const Login({super.key});
@@ -9,23 +10,65 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  final user = TextEditingController();
+  final pwd = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
-      body: Center(
-        child: Column(
-          children: [
-            const SizedBox(height: 150),
-            Text(
-              'Welcome to TYR',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 28,
-                fontFamily: GoogleFonts.lato().fontFamily,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              const SizedBox(height: 150),
+              Icon(
+                Icons.verified_user_rounded,
+                size: 100,
+                color: Colors.grey.shade900,
               ),
-            )
-          ],
+              const SizedBox(height: 50),
+              Text(
+                'Welcome to TYR',
+                style: TextStyle(
+                  color: Colors.grey[700],
+                  fontSize: 28,
+                  fontFamily: GoogleFonts.lato().fontFamily,
+                ),
+              ),
+              const SizedBox(height: 30),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                child: TheTextField(
+                  controller: user,
+                  hint: 'Username',
+                  obsecure: false,
+                ),
+              ),
+              const SizedBox(height: 30),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                child: TheTextField(
+                  controller: pwd,
+                  hint: 'Password',
+                  obsecure: true,
+                ),
+              ),
+              const SizedBox(height: 50),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  textStyle: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  backgroundColor: Colors.black,
+                  foregroundColor: Colors.white,
+                  padding: const EdgeInsets.fromLTRB(35, 16, 35, 16),
+                ),
+                child: const Text('Login'),
+              )
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/components/textfield.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class TheTextField extends StatelessWidget {
+  final controller;
+  final String hint;
+  final bool obsecure;
+
+  const TheTextField(
+      {super.key,
+      required this.controller,
+      required this.hint,
+      required this.obsecure});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+      child: TextFormField(
+        controller: controller,
+        decoration: InputDecoration(
+          enabledBorder: const OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.white),
+          ),
+          focusedBorder: const OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.white),
+          ),
+          hintText: hint,
+          hintStyle: const TextStyle(fontSize: 18),
+          fillColor: Colors.grey.shade200,
+          filled: true,
+        ),
+        obscureText: obsecure,
+        style: const TextStyle(fontSize: 18),
+      ),
+    );
+  }
+}
