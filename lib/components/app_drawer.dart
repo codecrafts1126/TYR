7339c56a13c3b6f0a4b2d92521435a51194c59f0
codecrafts1126 <codecrafts1126@gmail.com>
2023-08-13 23:20:40 +0500

    Added whole background blur with drawer

--- lib/components/app_drawer.dart
+++ lib/components/app_drawer.dart
@@ -59,100 +59,98 @@ class _AppdrawState extends State<Appdraw> {
         width: MediaQuery.of(context).size.width * 0.5,
         child: Drawer(
           backgroundColor: Colors.white.withOpacity(0.7),
-          child: ClipRRect(
-            child: BackdropFilter(
-              filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
-              child: ListView(
-                padding: const EdgeInsets.all(5),
-                children: <Widget>[
-                  const SizedBox(height: 70),
-                  SizedBox(
-                    height: 35,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          'Welcome: $username',
-                          style: TextStyle(
-                            fontSize: 22,
-                            fontFamily: GoogleFonts.alata().fontFamily,
-                          ),
+          child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+            child: ListView(
+              padding: const EdgeInsets.all(5),
+              children: <Widget>[
+                const SizedBox(height: 70),
+                SizedBox(
+                  height: 35,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Welcome: $username',
+                        style: TextStyle(
+                          fontSize: 22,
+                          fontFamily: GoogleFonts.alata().fontFamily,
                         ),
-                      ],
-                    ),
-                  ),
-                  const SizedBox(height: 25),
-                  ListTile(
-                    leading: const Icon(Icons.home_outlined),
-                    title: Text(
-                      'Home',
-                      style: TextStyle(
-                        fontSize: 22,
-                        fontFamily: GoogleFonts.alata().fontFamily,
                       ),
-                    ),
-                    onTap: () {
-                      Navigator.pop(context);
-                      Navigator.of(context).push(MaterialPageRoute(
-                          builder: ((context) => const Home())));
-                    },
+                    ],
                   ),
-                  ListTile(
-                    leading: const Icon(Icons.person_2_outlined),
-                    title: Text(
-                      'Profile',
-                      style: TextStyle(
-                        fontSize: 22,
-                        fontFamily: GoogleFonts.alata().fontFamily,
-                      ),
-                    ),
-                    onTap: () {
-                      Navigator.pop(context);
-                      Navigator.of(context).push(MaterialPageRoute(
-                          builder: ((context) => const CurrentUser())));
-                    },
-                  ),
-                  ListTile(
-                    leading: const Icon(Icons.settings_outlined),
-                    title: Text(
-                      'Settings',
-                      style: TextStyle(
-                        fontSize: 22,
-                        fontFamily: GoogleFonts.alata().fontFamily,
-                      ),
-                    ),
-                    onTap: () {},
-                  ),
-                  const Divider(
-                    // thickness: 1.5,
-                    color: Colors.black54,
-                  ),
-                  const SizedBox(height: 5),
-                  ListTile(
-                    leading: const Icon(Icons.info_outline),
-                    title: Text(
-                      'About',
-                      style: TextStyle(
-                        fontSize: 22,
-                        fontFamily: GoogleFonts.alata().fontFamily,
-                      ),
+                ),
+                const SizedBox(height: 25),
+                ListTile(
+                  leading: const Icon(Icons.home_outlined),
+                  title: Text(
+                    'Home',
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontFamily: GoogleFonts.alata().fontFamily,
                     ),
                   ),
-                  ListTile(
-                    leading: const Icon(Icons.exit_to_app_outlined),
-                    title: Text(
-                      'Logout',
-                      style: TextStyle(
-                        fontSize: 22,
-                        fontFamily: GoogleFonts.alata().fontFamily,
-                      ),
+                  onTap: () {
+                    Navigator.pop(context);
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: ((context) => const Home())));
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(Icons.person_2_outlined),
+                  title: Text(
+                    'Profile',
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontFamily: GoogleFonts.alata().fontFamily,
                     ),
-                    onTap: () async {
-                      signOut();
-                    },
                   ),
-                ],
-              ),
+                  onTap: () {
+                    Navigator.pop(context);
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: ((context) => const CurrentUser())));
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(Icons.settings_outlined),
+                  title: Text(
+                    'Settings',
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontFamily: GoogleFonts.alata().fontFamily,
+                    ),
+                  ),
+                  onTap: () {},
+                ),
+                const Divider(
+                  // thickness: 1.5,
+                  color: Colors.black54,
+                ),
+                const SizedBox(height: 5),
+                ListTile(
+                  leading: const Icon(Icons.info_outline),
+                  title: Text(
+                    'About',
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontFamily: GoogleFonts.alata().fontFamily,
+                    ),
+                  ),
+                ),
+                ListTile(
+                  leading: const Icon(Icons.exit_to_app_outlined),
+                  title: Text(
+                    'Logout',
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontFamily: GoogleFonts.alata().fontFamily,
+                    ),
+                  ),
+                  onTap: () async {
+                    signOut();
+                  },
+                ),
+              ],
             ),
           ),
         ),
